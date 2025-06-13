8cde032f6d0d4c07d1424e99031db383fe8173cd
MirandaBiel <gabrieldaconceicaomiranda@gmail.com>
2025-06-13 09:36:44 -0300

    gráficos ruim

--- controlador_cal.m
+++ controlador_cal.m
@@ -1,41 +1,41 @@
 clc; clear; close all;
 
-%% 1. Parâmetros fornecidos
+% 1. Parâmetros fornecidos
 a = 11.661;
 b = 1.515;
 Mp = 3.25;     % Sobressinal (%)
 tp = 0.15;     % Tempo de pico (s)
 N = 20;        % Número de amostras por ciclo
 
-%% 2. Cálculo de parâmetros do sistema de 2ª ordem
+% 2. Cálculo de parâmetros do sistema de 2ª ordem
 zeta = sqrt(log(Mp/100)^2 / (pi^2 + log(Mp/100)^2));
 wn = pi / (tp * sqrt(1 - zeta^2));
-wd = wn * sqrt(1 - zeta^2);
-sigma = zeta * wn;
+wd = wn * sqrt(1 - zeta^2)
+sigma = zeta * wn
 
-%% 3. Cálculo do tempo de amostragem
-T = 2*pi / (N * wd);
+% 3. Cálculo do tempo de amostragem
+T = 2*pi / (N * wd)
 
-%% 4. Polos desejados no plano z
+% 4. Polos desejados no plano z
 s_d = -sigma + 1j*wd;
 z_d = exp(s_d*T);   % Mapeamento s -> z
-z_d_conj = conj(z_d);
+z_d_conj = conj(z_d)
 
-%% 5. Planta contínua e discretização
+% 5. Planta contínua e discretização
 s = tf('s');
 G_s = b / (s*(s+a));
-G_z = c2d(G_s, T, 'zoh');
+G_z = c2d(G_s, T, 'zoh')
 
-%% 6. Coeficientes da planta G(z)
+% 6. Coeficientes da planta G(z)
 [numGz, denGz] = tfdata(G_z, 'v');
-Gz = G_z;
+Gz = G_z
 
-%% 7. Cancelamento de polo (escolha do zero do controlador)
+% 7. Cancelamento de polo (escolha do zero do controlador)
 p1 = roots(denGz);   % polos da planta em z
 z_c = p1(1);         % zero do controlador cancela esse polo
 
-%% 8. Condição de fase: cálculo do polo do controlador
-z = z_d; % z_d é o polo desejado
+% 8. Condição de fase: cálculo do polo do controlador
+z = z_d % z_d é o polo desejado
 
 % Ângulos das contribuições dos pólos e zero existentes
 theta_G = angle(z - p1(1)) + angle(z - p1(2)) - angle(z - z_c);
@@ -47,19 +47,19 @@ real_z = real(z);
 theta_p = phi_required;
 p_c = real_z - imag_z / tan(theta_p);  % z - p_c forma o ângulo necessário
 
-%% 9. Condição de módulo: cálculo do ganho K
+% 9. Condição de módulo: cálculo do ganho K
 num_eval = abs(polyval(numGz, z) * (z - z_c));
 den_eval = abs(polyval(denGz, z) * (z - p_c));
 K = den_eval / num_eval;
 
-%% 10. Controlador digital Gc(z)
+% 10. Controlador digital Gc(z)
 z_tf = tf('z', T);
-Gc = K * (z_tf - z_c) / (z_tf - p_c);
+Gc = K * (z_tf - z_c) / (z_tf - p_c)
 
-%% 11. Sistema em malha fechada
+% 11. Sistema em malha fechada
 T_total = feedback(Gc*Gz, 1);
 
-%% 12. Simulações
+% 12. Simulações
 t = 0:T:5;
 
 % Degrau
@@ -76,7 +76,7 @@ plot(t_out, u, '--k');
 title('Resposta à Onda Quadrada');
 legend('Saída', 'Entrada');
 
-%% 13. LGR com marcação dos polos desejados
+% 13. LGR com marcação dos polos desejados
 figure;
 rlocus(Gc * Gz);
 hold on;
